624cdba127488a2f7e4b06f90e0a447f12c56d9e
SeungHyun Chae <seunghyunchae@SeungHyuns-MacBook-Air.local>
2020-08-29 21:42:09 +0900

    1546 solved

---
+++ 1546.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> scores(N);
+
+    int i, maxScore(0);
+    for(i = 0; i < N; i++)
+    {
+        cin >> scores[i];
+
+        if(maxScore < scores[i])
+        {
+            maxScore = scores[i];
+        }
+    }
+
+    double sum(0.0);
+    for(i = 0; i < N; i++)
+    {
+        sum += (double) scores[i] / maxScore * 100;
+    }
+
+    cout << sum / N << endl;
+
+    return 0;
+}
